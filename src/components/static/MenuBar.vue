b1275a1d52296b209d50483a181020191c6d52ce
Nico Kempe <accounts@nicokempe.de>
2022-12-20 00:09:44 +0100

    Add the menu bar component

---
+++ src/components/static/MenuBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li>
+      <RouterLink to="/news">News</RouterLink>
+      <i class="fa fa-newspaper w-4 h-4"></i>
+    </li>
+    <li>
+      <RouterLink to="/explore">Explore</RouterLink>
+      <i class="fa fa-magnifying-glass w-4 h-4"></i>
+    </li>
+    <li>
+      <RouterLink to="/notifications">Notifications</RouterLink>
+      <i class="fa fa-bell w-4 h-4"></i>
+    </li>
+    <li>
+      <RouterLink to="/profile">Profile</RouterLink>
+      <i class="fa fa-user w-4 h-4"></i>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "MenuBar"
+}
+</script>
