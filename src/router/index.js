baa6f62fc25c882c2eaf616ff55f5a0e8c28211c
Nico Kempe <accounts@nicokempe.de>
2022-12-20 00:08:41 +0100

    Update the router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,54 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import app from "@/main";
 
 const routes = [
+  /*
+  * Routes:
+  * /auth/login
+  * /auth/login/2fa
+  * /auth/login/forget-password
+  * /news/$pageId
+  * /news/article/$newsId
+  * /profile
+  * /profile/settings
+  * /profile/$userName
+  * */
   {
+    name: 'NewsRedirection',
     path: '/',
-    redirect: '/login'
+    redirect: '/news',
   },
   {
-path: '/login',
-    component: () => import('@/views/Login.vue')
-  }
+    name: 'NewsView',
+    path: '/news',
+    component: () => import('@/views/NewsView.vue')
+  },
+  {
+    name: 'LoginView',
+    path: '/auth/login',
+    component: () => import('@/views/auth/LoginView.vue'),
+    meta: {
+      auth: false,
+      menubar: false
+    }
+  },
+  {
+    name: '2faView',
+    path: '/auth/login/2fa',
+    component: () => import('@/views/auth/2faView.vue'),
+    meta: {
+      auth: false,
+      menubar: false
+    }
+  },
+  {
+    name: 'ForgetPasswordView',
+    path: '/auth/login/forget-password',
+    component: () => import('@/views/auth/ForgetPasswordView.vue'),
+    meta: {
+      auth: false,
+      menubar: false
+    }
+  },
 ]
 
 const router = createRouter({
