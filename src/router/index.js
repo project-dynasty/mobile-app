20084f1a486c1aa086b5a90b3e471c26baba0750
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-12-19 18:55:41 +0100

    Update the router configuration
    * Remove unwanted content
    * Add login path

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,13 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import TabsPage from '../views/TabsPage.vue'
 
 const routes = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/login'
   },
   {
-    path: '/tabs/',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      }
-    ]
+path: '/login',
+    component: () => import('@/views/Login.vue')
   }
 ]
 
