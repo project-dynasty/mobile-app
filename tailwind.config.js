5437cffa5be8c69994fe583a304f4130596234a8
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-12-19 18:54:57 +0100

    Add TailwindCSS, TailwindCSS Plugins, Axios and Capacitor Resources
    * Add TailwindCSS
    * Add TailwindCSS Plugins:
      * Aspect Ratio
      * Forms
      * Line Clamp
      * Typography
    * Add Axios
    * Add Ionic Storage
    * Add AutoPrefixer (for TailwindCSS)
    * Add PostCSS (for TailwindCSS)

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
