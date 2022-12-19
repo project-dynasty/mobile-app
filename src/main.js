22ea91e9ec01516dc9caa1ad440544caad58239a
xLikeAlex <git@alexanderwodarz.de>
2022-12-20 00:06:20 +0100

    Import plugins and configure Axios

--- src/main.js
+++ src/main.js
@@ -1,28 +1,42 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router';
 
-import { Storage } from '@ionic/storage'
-export const store = new Storage();
-
+import {Storage} from '@ionic/storage'
 import VueAxios from "vue-axios";
 import axios from "axios";
 import auth from "@/plugins/auth";
+import news from "@/plugins/news";
+import {IonicVue} from '@ionic/vue';
 
-import { IonicVue } from '@ionic/vue';
 
 /* TailwindCSS */
 import "@/assets/styles/tailwind.css";
+import device from "@/plugins/device";
+
+export const store = new Storage();
+
+axios.defaults.baseURL = 'https://tcp-api.project-dynasty.com'
+axios.interceptors.request.use(async function (config) {
+    const token = await store.get('token')
+    if (token !== null)
+        config.headers["Authorization"] = "Bearer " + await store.get('token');
+    return config;
+});
+
 
 const app = createApp(App)
-export default app
-app.use(IonicVue)
-app.use(router);
-app.use(auth)
-app.use(VueAxios, axios)
 
+export default app
+
+app.use(IonicVue)
+app.use(VueAxios, axios)
+app.use(router)
+app.use(auth)
+app.use(device)
+app.use(news)
 store.create().then(async () => {
-  router.isReady().then(() => {
-    app.mount('#app');
-  });
+    router.isReady().then(async () => {
+        app.mount('#app');
+    });
 })
