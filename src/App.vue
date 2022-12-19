01a1c851de5ebf4baccf9f4b6774ac7416b1d8d7
xLikeAlex <git@alexanderwodarz.de>
2022-12-20 00:07:19 +0100

    Add the menu bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
+<!--
+  TODO: Add Menu Bar
+ -->
 <template>
   <ion-app>
     <router-view/>
+    <MenuBar v-if="menuBarVisible"></MenuBar>
   </ion-app>
 </template>
 
@@ -12,7 +16,26 @@ import MenuBar from "@/components/static/MenuBar";
 
 export default defineComponent({
   name: 'App',
+  data() {
+    return {
+      menuBar: true
+    }
+  },
+  computed: {
+    menuBarVisible() {
+      if (!this.$route.meta) {
+        console.log("first", !this.$route.meta)
+        return true
+      }
+      if (this.$route.meta.menubar === undefined) {
+        console.log("second", !this.$route.meta.menuBar)
+        return true
+      }
+      return this.$route.meta.menubar
+    }
+  },
   components: {
+    MenuBar,
     IonApp
   }
 });
