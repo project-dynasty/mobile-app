d6d1e0905d23f25f6f8a20483ea0c1822788a207
Alexander Wodarz <git@alexanderwodarz.de>
2022-12-20 23:21:43 +0100

    Add automated paddings for iPhones

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@ import {IonApp} from '@ionic/vue';
 import {defineComponent} from 'vue';
 import "@/assets/styles/tailwind.css";
 import MenuBar from "@/components/static/MenuBar";
+import {Capacitor} from "@capacitor/core";
+import {Device} from "@capacitor/device";
 
 export default defineComponent({
   name: 'App',
@@ -21,14 +23,27 @@ export default defineComponent({
       menuBar: true
     }
   },
+  async mounted() {
+    if (Capacitor.getPlatform() === 'ios') {
+      const app = document.getElementById("app")
+      const info = await Device.getInfo()
+      if (info.model.toString().startsWith("iPhone")) {
+        const model = info.model.toString().replaceAll("iPhone", "")
+        if (model.split(",")[0] >= '15')
+          app.style = "padding-top: 50px"
+        else if (model.split(",")[0] >= '10' && model !== '14,6' && model !== '12,8')
+          app.style = "padding-top: 40px"
+        else
+          app.style = "padding-top: 20px"
+      }
+    }
+  },
   computed: {
     menuBarVisible() {
       if (!this.$route.meta) {
-        console.log("first", !this.$route.meta)
         return true
       }
       if (this.$route.meta.menubar === undefined) {
-        console.log("second", !this.$route.meta.menuBar)
         return true
       }
       return this.$route.meta.menubar
