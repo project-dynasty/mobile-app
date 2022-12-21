33e30d2f67dd5d2e9365f42edf1bec934dd92173
Alexander Wodarz <git@alexanderwodarz.de>
2022-12-21 22:27:25 +0100

    Add Sign In reset

--- src/plugins/auth.js
+++ src/plugins/auth.js
@@ -3,10 +3,37 @@ import {store} from "@/main";
 export default {
     install: async (app) => {
         const w = {
+            changeRoute: async to => {
+                const auth = await w.isAuthorized()
+                const requiresAuth = to.meta.auth === undefined ? true : to.meta.auth
+                if ((to.path === '/auth/sign-in' || to.path === '/auth/2fa') && auth)
+                    return {path: '/', replace: true}
+                if (requiresAuth && !auth)
+                    return {path: '/auth/sign-in', replace: true}
+                return true
+            },
+            confirm: async (otp) => {
+                try {
+                    const token = await store.get('signin_token')
+                    const response = await app.axios.post('/auth/auth', {token: token, code: otp, rememberMe: true})
+                    await w.setToken(response.data.token, response.data.expire)
+                    await this.resetSignIn()
+                    return {status: 'ok'}
+                } catch (e) {
+                    if (e.response.status === 400)
+                        return {status: 'failed', code: 400}
+                    return {status: 'failed'}
+                }
+            },
+            isAuthorized: async () => {
+                const expire = await store.get('expire')
+                const current = new Date().getTime()
+                return current < expire
+            },
             login: async (username, password) => {
                 try {
                     const response = await app.axios.post('/auth/signin', {username, password})
-                    if (response.data.expire === 0){
+                    if (response.data.expire === 0) {
                         await store.set('signin_token', response.data.token)
                         return {status: '2fa'}
                     }
@@ -21,35 +48,12 @@ export default {
             logout: async () => {
                 await store.clear()
             },
-            confirm: async (otp) => {
-                try {
-                    const token = await store.get('signin_token')
-                    const response = await app.axios.post('/auth/auth', {token: token, code: otp, rememberMe: true})
-                    await w.setToken(response.data.token, response.data.expire)
-                    return {status: 'ok'}
-                } catch (e) {
-                    if (e.response.status === 400)
-                        return {status: 'failed', code: 400}
-                    return {status: 'failed'}
-                }
+            resetSignIn: async () => {
+                await store.remove('signin_token')
             },
             setToken: async (token, expire) => {
                 await store.set('token', token)
                 await store.set('expire', expire)
-            },
-            isAuthorized: async () => {
-                const expire = await store.get('expire')
-                const current = new Date().getTime()
-                return current < expire
-            },
-            changeRoute: async to => {
-                const auth = await w.isAuthorized()
-                const requiresAuth = to.meta.auth === undefined ? true : to.meta.auth
-                if ((to.path === '/auth/login' || to.path === '/auth/login/2fa') && auth)
-                    return {path: '/', replace: true}
-                if (requiresAuth && !auth)
-                    return {path: '/auth/login', replace: true}
-                return true
             },
         }
         app.config.globalProperties.$auth = w
