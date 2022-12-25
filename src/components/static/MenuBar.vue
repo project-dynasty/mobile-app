912520a98c7393abf99db89ccaf5ab37b13c3384
Alexander Wodarz <git@alexanderwodarz.de>
2022-12-26 00:08:59 +0100

    Add id to footer

--- src/components/static/MenuBar.vue
+++ src/components/static/MenuBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-white shadow-lg w-full dark:bg-gray-800 dark:text-gray-100">
+  <footer id="footer" class="bg-white shadow-lg w-full dark:bg-gray-800 dark:text-gray-100">
     <nav class="flex items-center justify-around p-4 dark:hover:text-gray-200">
       <RouterLink to="/home" class="text-gray-600 dark:text-gray-100 dark:hover:text-gray-200 w-full md:w-1/4 px-2 py-1 text-center">
         <div class="flex items-center justify-center my-2 text-xl">
