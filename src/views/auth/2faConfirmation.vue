133cbbd634ab867ae771c9dc757d392182d324f5
Alexander Wodarz <git@alexanderwodarz.de>
2022-12-27 18:33:54 +0100

    Add Two Factor confirmation View

---
+++ src/views/auth/2faConfirmation.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <a>Login Bestätigen</a>
+    <div class="text-center mt-10">
+      <a class="m-10 p-5 rounded bg-cyan-300 cursor-pointer" v-for="number in numbers" :key="number"
+         @click="confirm(number)">{{ number }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import app from "@/main";
+
+export default {
+  data() {
+    return {
+      numbers: [],
+      token: ''
+    }
+  },
+  methods: {
+    async confirm(number) {
+      try {
+        const response = await app.axios.post('/auth/mobile-auth?token=' + this.token + '&confirmCode=' + number);
+        alert("Status: "+response.status)
+      } catch (e) {
+        alert("Fehler: "+e.response.status)
+      }
+      this.$router.push('/')
+    }
+  },
+  async mounted() {
+    if (!this.$route.query.token || !this.$route.query.numbers) {
+      this.$router.push('/')
+      return
+    }
+    const {status} = await this.$auth.checkConfirmStatus(this.$route.query.token)
+    if (status === 'error') {
+      this.$router.push('/')
+      return
+    }
+    this.numbers = this.$route.query.numbers.split(",")
+    this.token = this.$route.query.token
+  }
+}
+</script>
