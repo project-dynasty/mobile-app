b6e5781374ee89ed1518cd85bd3328b6866a5226
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-12-30 14:57:16 +0100

    Delete the news views

--- src/views/news/NewsArticleView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>News Article</h1>
-</template>
-
-<script>
-export default {
-  name: "NewsArticleView"
-}
-</script>
--- src/views/news/NewsListView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>News List</h1>
-</template>
-
-<script>
-export default {
-  name: "NewsListView"
-}
-</script>
