096f9e636e911ebaf8a44776596eb4a02a2a1930
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-12-30 16:39:28 +0100

    Add the settings icon

--- src/views/profile/ProfileView.vue
+++ src/views/profile/ProfileView.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="flex h-screen bg-gray-50 dark:bg-gray-900">
-    <div class="px-5 py-5">
-      <p class="text-xl font-semibold text-gray-900 dark:text-gray-300">Profile</p>
+    <div class="px-5 py-5 flex">
+      <div class="flex-col">
+        <p class="text-xl font-semibold text-gray-900 dark:text-gray-300">Profile</p>
+      </div>
+      <div class="flex-col">
+        <RouterLink to="/profile/settings">
+          <i class="fa fa-gear h-4 w-4 text-gray-900 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-400" />
+        </RouterLink>
+      </div>
     </div>
   </div>
 </template>
+
+
 
 <script>
 export default {
