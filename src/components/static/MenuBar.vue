102f7d019f645fe02a55aa570772c6adf6533e73
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-12-30 16:26:44 +0100

    Update the MenuBar style

--- src/components/static/MenuBar.vue
+++ src/components/static/MenuBar.vue
@@ -1,29 +1,25 @@
 <template>
-  <footer id="footer" class="bg-gray-200 shadow-lg w-full dark:bg-gray-900 dark:text-gray-100">
+  <footer id="footer" class="bg-gray-200 shadow-lg w-full dark:bg-gray-800 dark:text-gray-100 rounded-t-2xl opacity-90">
     <nav class="flex items-center justify-around p-4 dark:hover:text-gray-200">
-      <RouterLink to="/home" class="text-gray-600 dark:text-gray-100 dark:hover:text-gray-200 w-full md:w-1/4 px-2 py-1 text-center">
+      <RouterLink to="/home" class="text-gray-600 dark:text-gray-100 dark:hover:text-gray-200 w-full md:w-1/4 px-2 py-1 text-center opacity-100">
         <div class="flex items-center justify-center my-2 text-xl">
           <i class="fas fa-home"></i>
         </div>
-        <!-- <span class="text-base ml-2">Homepage</span> -->
       </RouterLink>
-      <RouterLink to="/explore" class="text-gray-600 dark:text-gray-100 dark:hover:text-gray-200 w-full md:w-1/4 px-2 py-1 text-center">
+      <RouterLink to="/explore" class="text-gray-600 dark:text-gray-100 dark:hover:text-gray-200 w-full md:w-1/4 px-2 py-1 text-center opacity-100">
         <div class="flex items-center justify-center my-2 text-xl">
           <i class="fas fa-magnifying-glass"></i>
         </div>
-        <!-- <span class="text-base ml-2">Explore</span> -->
       </RouterLink>
-      <RouterLink to="/notifications" class="text-gray-600 dark:text-gray-100 dark:hover:text-gray-200 w-full md:w-1/4 px-2 py-1 text-center">
+      <RouterLink to="/notifications" class="text-gray-600 dark:text-gray-100 dark:hover:text-gray-200 w-full md:w-1/4 px-2 py-1 text-center opacity-100">
         <div class="flex items-center justify-center my-2 text-xl">
           <i class="fas fa-bell"></i>
         </div>
-        <!-- <span class="text-base ml-2">Notifications</span> -->
       </RouterLink>
-      <RouterLink to="/profile" class="text-gray-600 dark:text-gray-100 dark:hover:text-gray-200 w-full md:w-1/4 px-2 py-1 text-center">
+      <RouterLink to="/profile" class="text-gray-600 dark:text-gray-100 dark:hover:text-gray-200 w-full md:w-1/4 px-2 py-1 text-center opacity-100">
         <div class="flex items-center justify-center my-2 text-xl">
           <i class="fas fa-user-circle"></i>
         </div>
-        <!-- <span class="text-base ml-2">Your Profile</span> -->
       </RouterLink>
     </nav>
   </footer>
