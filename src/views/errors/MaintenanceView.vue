b85aac6264741936a553b3e3d833f02fa93ba2fa
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-12-30 14:56:50 +0100

    Update maintenance styling

--- src/views/errors/MaintenanceView.vue
+++ src/views/errors/MaintenanceView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex h-screen">
-    <div class="m-auto">
+    <div class="m-auto px-5 py-5">
       <h1 class="font-bold">Maintenance</h1>
-      <p v-if="fullPageDevelopment" class="font-semibold">This App is currently in maintenance. Try again later.</p>
-      <p v-else class="font-semibold">This page is currently under development. Try again later.</p>
+      <p v-if="fullPageDevelopment" class="font-semibold">This App is currently in maintenance.</p>
+      <p v-else class="font-semibold">This page is currently under development.</p>
     </div>
   </div>
 </template>
