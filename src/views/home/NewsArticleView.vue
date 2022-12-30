dfd446c1b91bd931d068d8ce1aed3d2a347881e8
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-12-30 15:36:12 +0100

    Add the NewsArticleView prototype

---
+++ src/views/home/NewsArticleView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex h-screen bg-gray-50 dark:bg-gray-900">
+    <div class="px-5 py-5">
+      <p class="text-xl font-semibold text-gray-900 dark:text-gray-300">News Article</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewsArticleView",
+  data() {
+    return {
+      newsData: {
+        id: 0,
+        title: '',
+        description: '',
+        category: '',
+        bannerImg: '',
+        readingTime: '0min',
+        publishedDate: '2022-12-27',
+        author: {
+          name: '',
+          href: '',
+          icon: ''
+        },
+        intern: false,
+      },
+    }
+  }
+}
+</script>
