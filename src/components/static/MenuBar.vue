198fd7763df04f523a10d182c93a1f274792b440
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-12-30 17:16:23 +0100

    Add active navigation navbar color highlighting

--- src/components/static/MenuBar.vue
+++ src/components/static/MenuBar.vue
@@ -1,22 +1,22 @@
 <template>
   <footer id="footer" class="bg-gray-200 shadow-lg w-full dark:bg-gray-800 dark:text-gray-100 rounded-t-2xl opacity-90">
     <nav class="flex items-center justify-around p-4 dark:hover:text-gray-200">
-      <RouterLink to="/home" class="text-gray-600 dark:text-gray-100 dark:hover:text-gray-200 w-full md:w-1/4 px-2 py-1 text-center opacity-100">
+      <RouterLink to="/home" :class="{ 'text-cyan-600 dark:text-cyan-300': $route.path === '/home' }" class="text-gray-600 dark:text-gray-100 dark:hover:text-gray-200 w-full md:w-1/4 px-2 py-1 text-center opacity-100">
         <div class="flex items-center justify-center my-2 text-xl">
           <i class="fas fa-home"></i>
         </div>
       </RouterLink>
-      <RouterLink to="/explore" class="text-gray-600 dark:text-gray-100 dark:hover:text-gray-200 w-full md:w-1/4 px-2 py-1 text-center opacity-100">
+      <RouterLink to="/explore" :class="{ 'text-cyan-600 dark:text-cyan-300': $route.path === '/explore' }" class="text-gray-600 dark:text-gray-100 dark:hover:text-gray-200 w-full md:w-1/4 px-2 py-1 text-center opacity-100">
         <div class="flex items-center justify-center my-2 text-xl">
           <i class="fas fa-magnifying-glass"></i>
         </div>
       </RouterLink>
-      <RouterLink to="/notifications" class="text-gray-600 dark:text-gray-100 dark:hover:text-gray-200 w-full md:w-1/4 px-2 py-1 text-center opacity-100">
+      <RouterLink to="/notifications" :class="{ 'text-cyan-600 dark:text-cyan-300': $route.path === '/notifications' }" class="text-gray-600 dark:text-gray-100 dark:hover:text-gray-200 w-full md:w-1/4 px-2 py-1 text-center opacity-100">
         <div class="flex items-center justify-center my-2 text-xl">
           <i class="fas fa-bell"></i>
         </div>
       </RouterLink>
-      <RouterLink to="/profile" class="text-gray-600 dark:text-gray-100 dark:hover:text-gray-200 w-full md:w-1/4 px-2 py-1 text-center opacity-100">
+      <RouterLink to="/profile" :class="{ 'text-cyan-600 dark:text-cyan-300': $route.path === '/profile' }" class="text-gray-600 dark:text-gray-100 dark:hover:text-gray-200 w-full md:w-1/4 px-2 py-1 text-center opacity-100">
         <div class="flex items-center justify-center my-2 text-xl">
           <i class="fas fa-user-circle"></i>
         </div>
