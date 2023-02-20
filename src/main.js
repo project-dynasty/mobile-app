8aa365616ebb70dea62017e85d1e94ffb9411268
Alexander Wodarz <git@alexanderwodarz.de>
2023-02-20 10:46:04 +0100

    Add log for token refresh

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import VueAxios from "vue-axios";
 import axios from "axios";
 import auth from "@/plugins/auth";
 import news from "@/plugins/news";
+import infiniteScroll from 'vue-infinite-scroll'
+import {App as t} from "@capacitor/app";
+
+
 import {IonicVue} from '@ionic/vue';
 
 
@@ -19,8 +23,9 @@ export const store = new Storage();
 axios.defaults.baseURL = 'https://tcp-api.project-dynasty.com'
 axios.interceptors.request.use(async function (config) {
     const token = await store.get('token')
-    if (token !== null)
-        config.headers["Authorization"] = "Bearer " + await store.get('token');
+    if (token !== null) {
+        config.headers["Authorization"] = "Bearer " + token;
+    }
     return config;
 });
 
@@ -35,8 +40,33 @@ app.use(router)
 app.use(auth)
 app.use(device)
 app.use(news)
+
+app.use(infiniteScroll)
+
+t.addListener('appUrlOpen', (event) => {
+    // Example url: https://beerswift.app/tabs/tabs2
+    // slug = /tabs/tabs2
+    console.log(event.url)
+});
+
 store.create().then(async () => {
+    const refresh = await store.get('refresh')
+    let logout = false
+    if (refresh != null && refresh.length > 0) {
+        const renew = await app.$auth.getExpireDate() - Math.floor(new Date().getTime() / 1000)
+        if (renew < 15) {
+            const response = await app.$auth.refresh()
+            if (!response)
+                logout = true
+        }
+        await app.$auth.startRefreshInterval()
+    }
     router.isReady().then(async () => {
+        if (logout) {
+            alert("Du wurdest ausgeloggt. Die App wurde zulange nicht geöffnet.")
+            app.$auth.logout()
+            app.$router.push('/')
+        }
         app.mount('#app');
     });
 })
