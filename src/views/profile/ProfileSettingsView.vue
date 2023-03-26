01949030a8d6bfd5e213ac017a085a0929fa4222
Alexander Wodarz <git@alexanderwodarz.de>
2023-03-26 10:54:29 +0200

    Add some demo debug stuff

--- src/views/profile/ProfileSettingsView.vue
+++ src/views/profile/ProfileSettingsView.vue
@@ -8,13 +8,16 @@
           Push Register
         </button>
         <br>
-        <div class="mt-3"></div>
       </div>
       <button @click="register"
-              class="inline-flex items-center rounded-md border border-transparent bg-cyan-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-cyan-700 focus:ring-transparent dark:bg-cyan-400 dark:text-gray-100 dark:border-gray-700 dark:hover:bg-cyan-500 dark:focus:ring-transparent">
+              class="mt-3 inline-flex items-center rounded-md border border-transparent bg-cyan-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-cyan-700 focus:ring-transparent dark:bg-cyan-400 dark:text-gray-100 dark:border-gray-700 dark:hover:bg-cyan-500 dark:focus:ring-transparent">
         Register
       </button>
       <br>
+      <button @click="signInWithApple"
+              class="mt-3 inline-flex items-center rounded-md border border-transparent bg-cyan-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-cyan-700 focus:ring-transparent dark:bg-cyan-400 dark:text-gray-100 dark:border-gray-700 dark:hover:bg-cyan-500 dark:focus:ring-transparent">
+        Mit Apple anmelden
+      </button>
       <div class="mt-3">
 
         <button @click="startScan"
@@ -24,19 +27,41 @@
 
       </div>
     </div>
+    <ModalConfirm :open="open" @reject="unclaim" @accept="solve"></ModalConfirm>
   </div>
 </template>
 
 <script>
 
 import {BarcodeScanner} from '@capacitor-community/barcode-scanner';
+import {ASAuthorizationAppleIDRequest, SignInWithApple} from "@awesome-cordova-plugins/sign-in-with-apple";
+import ModalConfirm from "@/components/ModalConfirm.vue";
 
 export default {
   name: "ProfileSettingsView",
-  mounted() {
-    this.$news.load()
+  components: {ModalConfirm},
+  async mounted() {
+    if (this.$route.query.qr) {
+      this.open = true
+      const result = await this.$auth.claimChallenge(this.$route.query.qr)
+      if(!result){
+        this.open = false
+        return
+      }
+      this.challenge = this.$route.query.qr
+    }
+  },
+  data() {
+    return {
+      open: false,
+      challenge: ""
+    }
   },
   methods: {
+    openModal(){
+      this.$auth.open()
+      console.log(this.$open)
+    },
     async startScan() {
       await BarcodeScanner.checkPermission({force: true});
       await BarcodeScanner.prepare()
@@ -58,7 +83,19 @@ export default {
         console.log(challenge)
         await this.$auth.claimChallenge(challenge.challenge)
         console.log("CLAIMED CHALLENGE")
+        this.challenge = challenge.challenge
+        this.open = true
       }
+    },
+    async unclaim() {
+      await this.$auth.unclaim(this.challenge)
+      this.challenge = ""
+      this.open = false
+    },
+    async solve(){
+      await this.$auth.solve(this.challenge)
+      this.challenge = ""
+      this.open = false
     },
     registerPush() {
       alert('Register Push!')
@@ -67,6 +104,12 @@ export default {
     register() {
       alert('Register!')
       this.$device.registerDevice()
+    },
+    async signInWithApple() {
+      const response = await SignInWithApple.signin([ASAuthorizationAppleIDRequest.ASAuthorizationScopeEmail])
+      console.info(response.authorizationCode)
+      console.log(response.email)
+      console.log(response)
     }
   }
 }
