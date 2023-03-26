09d760ade9820b1609961b6c8159af9f014b9753
Alexander Wodarz <git@alexanderwodarz.de>
2023-03-26 10:51:50 +0200

    Update device plugin

--- src/plugins/device.js
+++ src/plugins/device.js
@@ -1,9 +1,9 @@
 import {PushNotifications} from '@capacitor/push-notifications';
 import {Capacitor} from "@capacitor/core";
 import {Device} from '@capacitor/device';
+import {store} from "@/main";
 
-
-const addListeners = async (app) => {
+const addListeners = async (app, w) => {
     await PushNotifications.addListener('registration', token => {
         console.info('Registration token: ', token.value);
         console.log(app)
@@ -16,20 +16,33 @@ const addListeners = async (app) => {
         console.error('Registration error: ', err.error);
     });
 
-    await PushNotifications.addListener('pushNotificationReceived', notification => {
+    await PushNotifications.addListener('pushNotificationReceived', async notification => {
         console.log('Push notification received: ', notification);
+        console.log(await app.$auth.isAuthorized())
+        if (!await app.$auth.isAuthorized()) {
+            console.log("canceled because no auth")
+            return
+        }
         if (notification.data['2fa']) {
             const not = JSON.parse(notification.data['2fa'])
-            app.config.globalProperties.$router.push('/2fa?token=' + not.token + '&numbers=' + not.numbers)
+            //app.config.globalProperties.$router.push('/2fa?token=' + not.token + '&numbers=' + not.numbers)
+            console.log(app)
+            w.open()
+            app.$auth.openMultiFactor(not.token, not.numbers.split(","))
+
         }
     });
 
-    await PushNotifications.addListener('pushNotificationActionPerformed', notification => {
+    await PushNotifications.addListener('pushNotificationActionPerformed', async notification => {
         console.log(notification.notification.data)
         console.log('Push notification action performed', notification.actionId, notification.inputValue);
+        if (!await app.$auth.isAuthorized()) {
+            console.log("canceled because no auth")
+            return
+        }
         if (notification.actionId === 'tap' && notification.notification.data['2fa']) {
             const not = JSON.parse(notification.notification.data['2fa'])
-            app.config.globalProperties.$router.push('/2fa?token=' + not.token + '&numbers=' + not.numbers)
+            app.$auth.saveNotification(not.token, not.numbers.split(","))
         }
     });
 }
@@ -51,8 +64,30 @@ const registerNotifications = async () => {
 export default {
     install: async (app) => {
         const w = {
+            getAvatar: '',
+            getFirstname: '',
+            getLastname: '',
+            challengeOpen: false,
+            setUserInStore: async () => {
+                const auth = await app.$auth.isAuthorized()
+                if (!auth)
+                    return
+                try {
+                    const {data} = await app.axios.get('https://tcp-api.project-dynasty.com/account?id='+(app.$auth.parseJwt(await app.$auth.getToken()).id))
+                    await store.set('user', data)
+                    w.getAvatar = data.account.avatarBase64
+                    w.getFirstname = data.account.firstName
+                    w.getLastname = data.account.getLastName
+                } catch (e) {
+                    console.log(e)
+                    console.log("error on saveAvatarFromServer")
+                }
+            },
+            open: () => {
+                w.setUserInStore()
+            },
             pushTokenToServer: async (token) => {
-                const response = await app.axios.put('/device/device?token=' + token)
+                const response = await app.axios.post('https://api.project-dynasty.de/push/update', {token})
                 console.log(response)
             },
             registerPush: () => {
@@ -66,7 +101,7 @@ export default {
             }
         }
         if (Capacitor.getPlatform() !== "web")
-            await addListeners(app)
+            await addListeners(app, w)
         app.config.globalProperties.$device = w
         app.$device = w
     }
